982bb23c8ff09f37881925dfd0a21b8ead14b346
ykk648 <ykk648@gmail.com>
2024-02-07 17:48:31 +0800

    [scrfd tracking]

--- face_detect/scrfd_insightface/scrfd_api.py
+++ face_detect/scrfd_insightface/scrfd_api.py
@@ -1,10 +1,11 @@
 # -- coding: utf-8 --
 # @Time : 2023/12/22
 # @Author : ykk648
-# @Project : https://github.com/ykk648/AI_power
+
 import numpy as np
 import cv2
 from cv2box import CVImage
+from cv2box.utils.math import Normalize
 
 from .scrfd import SCRFD
 
@@ -23,14 +24,22 @@ MODEL_ZOO = {
 
 
 class ScrfdAPI:
-    def __init__(self, model_name='scrfd_500m_bnkps_shape640x640', provider='gpu', dynamic_shape=None):
+    def __init__(self, model_name='scrfd_500m_bnkps_shape640x640', provider='gpu', dynamic_shape=None, tracking=False,
+                 tracking_ratio=0.5):
         self.model = SCRFD(model_file=MODEL_ZOO[model_name]['model_path'])
         ctx_id = 0 if provider == 'gpu' else -1
-        self.model.prepare(ctx_id=ctx_id, input_size=dynamic_shape if dynamic_shape else MODEL_ZOO[model_name]['input_dynamic_shape'])
+        self.model.prepare(ctx_id=ctx_id,
+                           input_size=dynamic_shape if dynamic_shape else MODEL_ZOO[model_name]['input_dynamic_shape'])
 
         self.image = None
         self.bboxes = None
         self.kpss = None
+
+        # tracking one face
+        self.tracking = tracking
+        self.tracking_ratio = tracking_ratio
+        self.dis_list = []
+        self.last_bboxes_ = None
 
     def forward(self, image_in, nms_thresh=0.5, max_num=0):
         """
@@ -43,9 +52,37 @@ class ScrfdAPI:
             kpss: (N,5,2) if max_num=0
         """
         self.image = CVImage(image_in).rgb()
-        self.bboxes, self.kpss = self.model.detect_faces(self.image, thresh=nms_thresh, max_num=max_num,
-                                                         metric='default')
+
+        if self.tracking:
+            if self.last_bboxes_ is None:
+                self.bboxes, self.kpss = self.model.detect_faces(self.image, thresh=nms_thresh, max_num=1,
+                                                                 metric='default')
+                self.last_bboxes_ = self.bboxes
+            else:
+                self.bboxes, self.kpss = self.model.detect_faces(self.image, thresh=nms_thresh, max_num=0,
+                                                                 metric='default')
+                self.bboxes, self.kpss = self.tracking_filter()
+        else:
+            self.bboxes, self.kpss = self.model.detect_faces(self.image, thresh=nms_thresh, max_num=max_num,
+                                                             metric='default')
+
         return self.bboxes, self.kpss
+
+    def tracking_filter(self):
+        if len(self.bboxes) == 0 or len(self.last_bboxes_) == 0:
+            return None, None
+        for i in range(len(self.bboxes)):
+            self.dis_list.append(
+                np.linalg.norm(Normalize(self.bboxes[i]).np_norm() - Normalize(self.last_bboxes_[0]).np_norm()))
+        if not self.dis_list:
+            return None, None
+        best_index = np.argmin(np.array(self.dis_list))
+        # print(self.dis_list[best_index])
+        if self.dis_list[best_index] > self.tracking_ratio:
+            return None, None
+        self.dis_list = []
+        self.last_bboxes_ = np.array([self.bboxes[best_index]])
+        return self.last_bboxes_, np.array([self.kpss[best_index]])
 
     def draw_face(self):
         draw_image = self.image.copy()
@@ -60,6 +97,3 @@ class ScrfdAPI:
                     cv2.circle(draw_image, tuple(kp), 1, (0, 0, 255), 2)
         CVImage(CVImage(draw_image).rgb(), image_format='cv2').show()
         return draw_image
-
-
-
